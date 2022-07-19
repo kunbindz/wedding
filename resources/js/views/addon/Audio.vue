b940f29723a755937e3adb537b215e4bdd37d5cc
Tran Xuan Loc <tx.loc@arborsys.de>
2022-07-19 08:15:48 +0700

    update

---
+++ resources/js/views/addon/Audio.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <audio id="audio" muted autoplay loop>
+        <source src="assets/music/perfect.mp3">
+    </audio>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
